500072020b3631102f6aa5db2660cad65275553e
alabama56 <jakedownard@yahoo.com>
2017-09-18 15:09:11 -0500

    Assessment

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.btn').on('click', function() {
+        alert('Hello');
+    });
+});
+
+$(document).ready(function() {
+    $('button').on('click', function() {
+        alert('There is Nothing More');
+    });
+});
